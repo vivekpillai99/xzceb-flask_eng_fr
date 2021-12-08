82ed5c967bce52ca77dfc3004ec9493ece4dbeae
vivekpillai99 <vivekpillai@email.arizona.edu>
2021-12-08 17:42:56 +0000

    Slight change to server.py

--- final_project/server.py
+++ final_project/server.py
@@ -16,7 +16,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
